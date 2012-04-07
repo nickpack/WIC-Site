aa6bce45e4c6d3ab651c9df50fc9c0cb8e3ebc15
Nick Pack <nick@nickpack.com>
2012-04-07 22:38:10 +0100

    s/gigs/live

--- wicsite/urls.py
+++ wicsite/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^gigs/$', 'wic.views.gigs_index'),
+    url(r'^live/$', 'wic.views.gigs_index'),
     url(r'^media/$', 'wic.views.media_index'),
     url(r'^$', 'wic.views.main_index'),
 )
