46ff5ae13ff5de172ab01ba7652885984c06f7ea
Nick Pack <nick@nickpack.com>
2012-04-09 21:47:59 +0100

    Thumbnail fuckery

--- wic/admin.py
+++ wic/admin.py
@@ -1,10 +1,16 @@
 __author__ = 'Nick Pack'
 from wic.models import *
 from django.contrib import admin
+from imagekit.admin import AdminThumbnail
 
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'admin_thumbnail')
+    admin_thumbnail = AdminThumbnail(image_field='thumbnail')
+
+
+admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Gig)
 admin.site.register(BandMember)
 admin.site.register(Album)
 admin.site.register(Track)
-admin.site.register(Photo)
 admin.site.register(NewsArticle)
